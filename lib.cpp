992960a257debacdaec6e49cf8f72cdae24f1e95
Termi11 <91840662+Termi11@users.noreply.github.com>
2021-11-30 20:56:02 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+//
+// Created by Giulio on 29/11/2021.
+//
+#include "lib.h"
 
+bool conversione (char &a) {
+    bool t=true;
+    if (a>=65 and a<=90 or a>=97 and a<=122) {
+        t = true;
+        if (a >= 65 and a <= 90) {
+            a = a + 32;
+            return &a;
+        } else {
+            a = a - 32;
+            return &a;
+        }
+    }else {
+        t=false;
+    }
+    return t;
+}
